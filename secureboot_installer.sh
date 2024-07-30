e60fdef3dcde2cf9092d61393a54f4735a748c02
WeeXnes <somtrigi@gmail.com>
2024-07-30 04:07:03 +0200

    update

--- secureboot_installer.sh
+++ secureboot_installer.sh
@@ -21,18 +21,22 @@ update_signature_vanilla(){
     sudo sbsign --key /etc/refind.d/keys/refind_local.key --cert /etc/refind.d/keys/refind_local.crt --output /boot/vmlinuz-linux /boot/vmlinuz-linux
 }
 update_signature_cachyos(){
-    echo signing [/boot/vmlinuz-linux]
+    echo signing [/boot/vmlinuz-linux-cachyos-eevdf]
     sudo sbsign --key /etc/refind.d/keys/refind_local.key --cert /etc/refind.d/keys/refind_local.crt --output /boot/vmlinuz-linux-cachyos-eevdf /boot/vmlinuz-linux-cachyos-eevdf
 }
 
 add_cachyos_to_refind_config(){
+  echo this needs to be run as root/sudo
+  echo Adding Menu Entry for Cachy Kernel to refind.conf
   echo "menuentry \"CachyOS\" {" >> /boot/efi/EFI/refind/refind.conf
-  echo "        icon    /EFI/refind/icons/os_arch.png" >> /boot/efi/EFI/refind/refind.conf
+  echo "        icon    /EFI/refind/themes/refind-ambience/icons/os_arch.png" >> /boot/efi/EFI/refind/refind.conf
   echo "        volume  \"ROOT\"" >> /boot/efi/EFI/refind/refind.conf
   echo "        loader  /boot/vmlinuz-linux-cachyos-eevdf" >> /boot/efi/EFI/refind/refind.conf
   echo "        initrd  /boot/initramfs-linux-cachyos-eevdf.img" >> /boot/efi/EFI/refind/refind.conf
   echo "        options \"root=/dev/nvme0n1p2 rw  quiet splash loglevel=3 udev.log_level=3 vt.global_cursor_default=0 splash lsm=landlock,lockdown,yama,integrity,apparmor,bpf\"" >> /boot/efi/EFI/refind/refind.conf
   echo "}" >> /boot/efi/EFI/refind/refind.conf
+  echo done
+  exit
 }
 
 install_theme(){
@@ -44,7 +48,16 @@ install_theme(){
 }
 
 
-install_secureboot
-install_theme
-#update_signature_cachyos
-add_cachyos_to_refind_config
+
+if [ $1 = "install" ]; then
+  install_secureboot
+  install_theme
+elif [ $1 = "update" ]; then
+  update_signature_vanilla
+elif [ $1 = "update-cachy" ]; then
+  update_signature_cachyos
+elif [ $1 = "setup-cachy" ]; then
+  add_cachyos_to_refind_config
+else
+  echo "unknown command, avaiable commands: install, update, update-cachy, setup-cachy"
+fi
